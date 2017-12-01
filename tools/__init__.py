cac302ef069892045a3ab57fc0fcf539d1882dfb
Theo Olsthoorn <tolsthoorn@gmail.com>
2017-12-01 13:58:03 +0100

    GGOR
    GGOR is a simulation environment to analyse the desired ground and surface water regime in the Duch contect.
    A region is subdivided in possibly thousands of parcels given in a GIS database

---
+++ tools/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+GGOR package
+
+preferred usage
+    make sure that the GGOR/tools directory is asdded to sys.path
+    the you can load each module:
+
+    import mfgrid as grid
+
+Created on Thu Sep 22 00:35:12 2016
+
+@author: Theo 171201
+"""
+# Get these in mflab's namespace
+
+
+from .mfgrid import *
+from .ggor_tools import *
